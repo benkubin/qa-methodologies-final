ac7aff3f6efce08fc06df5ce4c17df29c62590ab
benkubin <103348532+benkubin@users.noreply.github.com>
2024-11-28 15:52:13 -0800

    created files, added canDrive function

---
+++ CanDrive.java
@@ -0,0 +1,16 @@
+public class CanDrive
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+
+    // canDrive function for testing
+    public static boolean canDrive(int age) {
+
+        int drivingAge = 16;
+        return age >= drivingAge;
+
+    }
+}
